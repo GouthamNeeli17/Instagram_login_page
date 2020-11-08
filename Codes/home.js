c005f2f2589b27de9c81e2f7f30a40ebee00882f
Goutham Neeli <68471620+GouthamNeeli17@users.noreply.github.com>
2020-11-08 11:28:26 +0530

    Add files via upload

---
+++ Codes/home.js
@@ -0,0 +1,8 @@
+
+function removeElement(){
+    document.body.style.backgroundColor = "black";   
+    document.body.innerHTML = 'Yeah!';
+    document.body.style.color = "red";
+    document.body.style.padding = "20% 46%";
+    document.body.style.fontSize = "25px";
+}
